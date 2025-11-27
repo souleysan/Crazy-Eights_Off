640eea76dcec7e31b33b60a05c148e1df571e7cb
souleymane <souleymane.sane@etu.u-paris.fr>
2025-11-27 20:04:58 +0100

    Tests (classiques) + compilation

---
+++ src/main/java/players/TestPlayers.java
@@ -0,0 +1,18 @@
+package players;
+
+public class TestPlayers {    
+    public static void main(String[] args) {
+
+        // Packet pG = new Packet(); pG.initPacketEntier();
+        // Player k = new  Player("sousou", pG, 4);
+        // Player d = new  Player();
+
+        // // k.getName(); k.getDeck();
+        // k.descrDeck();
+
+        // d.setName("okay"); d.setDeck(pG, 4);
+        // // d.getName(); d.getDeck();
+        // d.descrDeck();
+
+    }
+}
