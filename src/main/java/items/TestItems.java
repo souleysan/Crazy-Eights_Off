aa312694094834d72fb4bac2ece79429c806be50
souleymane <souleymane.sane@etu.u-paris.fr>
2025-11-27 17:37:22 +0100

    Neutralisation des tests (classiques)

--- src/main/java/items/TestItems.java
+++ src/main/java/items/TestItems.java
@@ -3,15 +3,15 @@ package items;
 public class TestItems {
     public static void main (String [] args) {
 
-        Carte sixCARREAU = new Carte(5,0); sixCARREAU.descr();
-        Carte roiCOEUR = new Carte(12,1); roiCOEUR.descr();
-        Carte valetPIQUE = new Carte(10,2); valetPIQUE.descr();
-        Carte dameTREFLE = new Carte(11,3); dameTREFLE.descr();
+//        Carte sixCARREAU = new Carte(5,0); sixCARREAU.descr();
+//        Carte roiCOEUR = new Carte(12,1); roiCOEUR.descr();
+//        Carte valetPIQUE = new Carte(10,2); valetPIQUE.descr();
+//        Carte dameTREFLE = new Carte(11,3); dameTREFLE.descr();
 
-        System.out.println("\n");
-        Packet p = new Packet(); p.initPacketEntier();
-        p.contenu(); // p.contenu(27);
-        Packet y = p.deckPlayer(4); y.contenu();
+//        System.out.println("\n");
+//        Packet p = new Packet(); p.initPacketEntier();
+//        p.contenu(); // p.contenu(27);
+//        Packet y = p.deckPlayer(4); y.contenu();
         
     }
 }
